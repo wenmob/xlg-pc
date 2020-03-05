26b8d0f10a575693f588e5c23bcb382f34e3cd26
wenmob <372193233@qq.com>
2020-03-05 11:54:02 +0800

    Add files via upload

---
+++ src/vue.config.js
@@ -0,0 +1,12 @@
+const proxy = require("./proxy.config");
+module.exports = {
+  pluginOptions: {},
+  lintOnSave: false,
+  devServer: {
+    // host: "0.0.0.0",
+    port: "9527",
+    open: true,
+    disableHostCheck: true,
+    proxy
+  }
+};
